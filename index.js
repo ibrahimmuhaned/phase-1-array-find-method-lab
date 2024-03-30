1732f3b5528f1b34b6549def2120db15f6687177
ibrahimmuhaned <ibrahimmuhaned999@gmail.com>
2024-03-30 22:42:52 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+
+const superbowlWin = (record) => {
+    const win = record.find(game => game.result === "W");
+    return win ? win.year : undefined;
+  }
+  
+  module.exports = superbowlWin;
+  
